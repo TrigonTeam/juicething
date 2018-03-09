f6cf773717812ae86f6ae7918302f75bbb0556c3
Jaroslav Boudný <jboudny0@gmail.com>
2018-03-09 23:47:52 +0100

    Add fixed timestep

--- core/src/eu/trigon/juice/JuiceGame.java
+++ core/src/eu/trigon/juice/JuiceGame.java
@@ -10,13 +10,19 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class JuiceGame extends ApplicationAdapter {
     SpriteBatch batch;
     ShapeRenderer shape;
     BitmapFont font;
     GlyphLayout l;
-    final String msg = "Ahoj, šotoušku :)";
+
+    private int tps = 20, nanosPerSec = 1000000000;
+    private double tickTime = 1d / tps;
+    private double tickTimeSec = this.tickTime * this.nanosPerSec;
+    private long time, lastTime;
+    private int ticks;
 
     @Override
     public void create() {
@@ -27,20 +33,39 @@ public class JuiceGame extends ApplicationAdapter {
         FreeTypeFontGenerator.FreeTypeFontParameter p = new FreeTypeFontGenerator.FreeTypeFontParameter();
         p.size = 120;
         p.color = Color.WHITE;
-        p.characters = msg;
+        //p.characters = "123456789";
         p.borderColor = Color.BLACK;
         p.borderWidth = 2;
         this.font = generator.generateFont(p);
         generator.dispose();
 
         this.l = new GlyphLayout();
-        l.setText(this.font, this.msg);
+
+        this.time = this.lastTime = TimeUtils.nanoTime();
     }
 
     float h = 0;
 
     @Override
     public void render() {
+        float ptt = (this.time - this.lastTime) / ((float) this.tickTimeSec);
+
+        renderTick(this.ticks, ptt);
+
+        this.time = TimeUtils.nanoTime();;
+        while (time - lastTime >= this.tickTimeSec) {
+            this.ticks++;
+            tick();
+
+            lastTime += this.tickTimeSec;
+        }
+    }
+
+    private void tick() {
+
+    }
+
+    private void renderTick(int tick, float ptt) {
         Color c = new Color();
         c.fromHsv(h, 0.8f, 1);
 
@@ -52,17 +77,14 @@ public class JuiceGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(c.r, c.g, c.b, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        String msg = Integer.toString(ticks);
+        this.l.setText(this.font, msg);
+
         batch.begin();
-        this.font.draw(batch, this.msg, Gdx.graphics.getWidth() / 2 - this.l.width / 2,
+        this.font.draw(batch, msg, Gdx.graphics.getWidth() / 2 - this.l.width / 2,
                 Gdx.graphics.getHeight() / 2 + this.l.height / 2);
         batch.end();
 
-        /*shape.begin(ShapeRenderer.ShapeType.Line);
-        shape.setColor(Color.BLACK);
-        shape.rect(Gdx.graphics.getWidth() / 2 - this.l.width / 2,
-                Gdx.graphics.getHeight() / 2 - this.l.height / 2,
-                this.l.width, this.l.height);
-        shape.end();*/
     }
 
     @Override
