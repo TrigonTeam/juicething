1a20569c4d24967254c6ae85587675039f0c3557
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2018-03-10 15:53:32 +0100

    Move test code to a separate class

--- core/src/eu/trigon/juice/JuiceGame.java
+++ core/src/eu/trigon/juice/JuiceGame.java
@@ -20,33 +20,18 @@ import java.util.List;
 import java.util.Random;
 
 public class JuiceGame extends ApplicationAdapter {
-    SpriteBatch batch;
-    ShapeRenderer shape;
-    ImmediateModeRenderer surfRenderer;
-
-    FluidSurface fluidSurface;
-    private int waterHeight = 640;
-
-    private Random rand;
-    private List<FluidDrop> drops;
-
     private int tps = 60, nanosPerSec = 1000000000;
     private double tickTime = 1d / tps;
     private double tickTimeSec = this.tickTime * this.nanosPerSec;
     private long time, lastTime;
     private int ticks;
 
+    private MainScreen s;
+
     @Override
     public void create() {
-        this.batch = new SpriteBatch();
-        this.shape = new ShapeRenderer();
-        this.surfRenderer = new ImmediateModeRenderer20(false, true, 0);
-
-        this.fluidSurface = new FluidSurface(Gdx.graphics.getWidth() / 4, 0.04f, 0.015f, 0.4f, 6);
-        this.drops = new ArrayList<FluidDrop>();
-
-        this.rand = new Random();
         this.time = this.lastTime = TimeUtils.nanoTime();
+        this.s = new MainScreen();
     }
 
     @Override
@@ -65,89 +50,18 @@ public class JuiceGame extends ApplicationAdapter {
     }
 
     private void tick() {
-        this.fluidSurface.tick();
-
-        if (this.rand.nextInt(10) == 0) {
-            this.drops.add(new FluidDrop(rand.nextInt(Gdx.graphics.getWidth()), Gdx.graphics.getHeight() + 100, 0, 0, 10 + rand.nextInt(15)));
-        }
-
-        List<FluidDrop> splashDrops = new ArrayList<FluidDrop>();
-
-        Iterator<FluidDrop> it = this.drops.iterator();
-        while (it.hasNext()) {
-            FluidDrop d = it.next();
-            d.tick();
-
-            if (d.getVelY() > 0) {
-                continue;
-            }
-
-            if (d.getX() > Gdx.graphics.getWidth() || d.getX() < 0) {
-                it.remove();
-                continue;
-            }
-
-            int x = (int) d.getX();
-
-            double scale = this.fluidSurface.getSegCount() / (double) Gdx.graphics.getWidth();
-            x = (int) (scale * x);
-
-            if (d.getY() < this.fluidSurface.getSegHeight(x) + this.waterHeight - d.getSize() * 2) {
-                it.remove();
-
-                if (d.getSize() >= 5f) {
-
-                    int pCount = 2 + this.rand.nextInt(4);
-
-                    for (int i = 0; i < pCount; i++) {
-                        splashDrops.add(new FluidDrop(d.getX(), d.getY() - d.getSize(), (rand.nextFloat() - 0.5f) * d.getSize() / 2, d.getSize() + rand.nextFloat() * d.getSize() / 2, d.getSize() / (1.75f + this.rand.nextFloat() * 1.5f)));
-                    }
-
-                    this.fluidSurface.splash(x, -d.getSize() * 3);
-                }
-            }
-        }
-
-        this.drops.addAll(splashDrops);
+        this.s.tick();
     }
 
     private void renderTick(int tick, float ptt) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        float height = this.waterHeight;
-
-        int segCount = this.fluidSurface.getSegCount();
-        float step = Gdx.graphics.getWidth() / (float) (segCount - 1);
-
-        this.shape.begin(ShapeRenderer.ShapeType.Filled);
-
-        this.shape.setColor(0.42f, 0.65f, 1f, 1f);
-
-        for (FluidDrop d : this.drops) {
-            this.shape.circle(d.getRenderX(ptt), d.getRenderY(ptt), d.getSize(), 16);
-        }
-
-        this.shape.end();
-
-        this.surfRenderer.begin(this.batch.getProjectionMatrix(), GL20.GL_TRIANGLE_STRIP);
-        for (int i = 0; i < (segCount); i++) {
-            float x0 = i * step;
-            float y0 = this.fluidSurface.getRenderSegHeight(i, ptt) + height;
-
-            this.surfRenderer.color(0.0f, 0.38f, 1f, 1f);
-            this.surfRenderer.vertex(x0, 0, 0);
-            this.surfRenderer.color(0.42f, 0.65f, 1f, 1f);
-            this.surfRenderer.vertex(x0, y0, 0);
-        }
-        this.surfRenderer.end();
-
-        // batch.begin();
-        // batch.end();
+        this.s.renderTick(tick, ptt);
     }
 
     @Override
     public void dispose() {
-        this.batch.dispose();
+        this.s.dispose();
     }
 }
